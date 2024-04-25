a81b2c7a4d80e644bcdcd05b9c7bd4f5798510f4
Roy Hadad <roy@datree.io>
2024-04-25 17:24:19 +0800

    feat: display different content for signed out users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,36 @@
 import { db } from "~/server/db";
 import Image from "next/image";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (modal, { desc }) => desc(modal.name),
   });
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id}>
+          {image.name}
+          <Image width="192" height="192" src={image.url} alt="gallery item" />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      Hello gallery app!
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id}>
-            {image.name}
-            <Image
-              width="192"
-              height="192"
-              src={image.url}
-              alt="gallery item"
-            />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
